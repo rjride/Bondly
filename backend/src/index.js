969a9b33f901b53061fcea2cbe351bbbece7228d
Rahul <rajwanshirahul54@gmail.com>
2025-01-24 11:50:11 +0530

    add user schema model with essential attributes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 // const express = require("express");
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
 
+
+dotenv.config()
 const app = express();
 
-app.listen(5001, ()=>{
-    console.log("server is running on port: 5001"); 
-})
+const PORT = process.env.PORT
+ app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log("server is running on port: " +PORT); 
+    connectDB();
+});
