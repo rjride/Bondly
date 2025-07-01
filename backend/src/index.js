e559f61763407bf828db8139e8cf8316f85c774a
Rahul <rajwanshirahul54@gmail.com>
2025-07-01 13:44:20 +0530

    chore: serve frontend static files in production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,13 +7,16 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {app,server} from "./lib/socket.js";
-
+import path from "path";
 
 dotenv.config();
 
 
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
+
+
 app.use(express.json({ limit: '5mb' }));  // or higher if needed
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
@@ -27,6 +30,15 @@ app.use(cors({
 
  app.use("/api/auth", authRoutes);
  app.use("/api/messages", messageRoutes);
+
+ if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend")));
+ 
+ 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+  });
+}
 
 
 server.listen(PORT, ()=>{
